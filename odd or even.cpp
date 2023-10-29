399afe806d05e7b19551b58a0891dfacab7088e6
Zahid Mondal <147536154+Zahid9985@users.noreply.github.com>
2023-10-29 22:06:05 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{  
+	int a, rem;
+	printf("Enter an integer numbres:");
+	scanf("%d",&a);
+	rem=a%2;
+	if(rem==0){
+		printf("%d is an even number\n",rem);
+	}
+	else{
+		printf("%d is a odd number",rem);
+	}
+	
+
+return 0;
+}
+
+#include<stdio.h>
+int maina()
+{
+	
+}
+
